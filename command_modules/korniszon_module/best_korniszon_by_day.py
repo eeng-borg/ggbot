c8cca3a6b743be58bf6e67089dfded5a3d2141a9
JohanIngeborg <gordianGP@wp.pl>
2025-02-26 09:07:43 +0100

    Add input validation for day selection in best korniszon command, and date selection

--- command_modules/korniszon_module/best_korniszon_by_day.py
+++ command_modules/korniszon_module/best_korniszon_by_day.py
@@ -8,8 +8,28 @@ from utils.utilities import wait_find_input_and_send_keys, wait_find_and_return,
 
 def best_korniszon_by_day(driver: webdriver.Chrome, data: CommandData, leaderboard: Leaderboard):
 
+    input = data.get('input')
+    day_input = input
 
-    day_input = data.get('input')
+    string_when = ''
+
+
+    # check if input is a number, otherwise throw exception message
+    try:
+        if input == '':
+            day_input = datetime.now().day
+            string_when = 'dziś'
+
+        else:
+            day_input = int(input)
+            string_when = f"z {day_input} dnia tego miesiąca"
+    
+    except ValueError:
+        response = "Niepoprawny format <okok>, wprowadź pojedyńcze liczbę z jakiego dnia tego miesiąca chciałbyś topniszona, np /topniszon 5."
+        wait_find_input_and_send_keys(driver, 1, By.ID, "chat-text", response)
+
+
+
     response = ''
     
     for korniszon in reversed(leaderboard.leaderboard):
@@ -17,21 +37,24 @@ def best_korniszon_by_day(driver: webdriver.Chrome, data: CommandData, leaderboa
         # if input is empty, then lets assume that user wants the best one from today
         # if day_input == '':
 
-        if korniszon['time']['day'] == datetime.now().day:
-            if korniszon['time']['month'] == datetime.now().month:
-                if korniszon['time']['year'] == datetime.now().year:
+        if korniszon['time'] != None:
+            if korniszon['time']['day'] == day_input:
+                if korniszon['time']['month'] == datetime.now().month:
+                    if korniszon['time']['year'] == datetime.now().year:
 
-                    position = korniszon.get('position')
-                    input = korniszon.get('input')
-                    score = korniszon.get('score')
-                    user = korniszon.get('user')
+                        position = korniszon.get('position')
+                        input = korniszon.get('input')
+                        score = korniszon.get('score')
+                        user = korniszon.get('user')
 
-                    time = korniszon.get('time')
-                    hour = time.get('hour')
-                    minute = time.get('minute')
+                        time = korniszon.get('time')
+                        hour = time.get('hour')
+                        minute = time.get('minute')
 
-                    response = "Najlepszy dziś <paker>\n"
-                    response += f"{position}. {input} - {score} ({user}) o {hour}:{minute:02d}"
+
+
+                        response = f"Najlepszy {string_when} <paker>\n"
+                        response += f"{position}. {input} - {score} ({user}) o {hour}:{minute:02d}"
 
         # else:
         #     if korniszon['time']['day'] == day_input:
