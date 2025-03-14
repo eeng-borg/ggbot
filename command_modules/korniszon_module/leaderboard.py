0d86ef6e98ca247d3bc4b945809d797a26ab7f24
JohanIngeborg <gordianGP@wp.pl>
2025-03-14 20:07:00 +0100

    ranking fixed

--- command_modules/korniszon_module/leaderboard.py
+++ command_modules/korniszon_module/leaderboard.py
@@ -127,7 +127,7 @@ class Leaderboard:
 
         # remove everything except numbers and spaces
         offset = 1
-        limit = 10
+        limit = 9
 
         # remove everything exept for numbers and spaces, 
         numbers = "".join([char for char in data_input if char.isdigit() or char.isspace()])
@@ -195,8 +195,8 @@ class Leaderboard:
 
             self.leaderboard_is_displayed = True
 
-            offset = 1
-            limit = 10
+            offset = 0
+            limit = 9
 
             # if empty command
             if len(data_input) > 0: # input always return string, empty string acts like none
